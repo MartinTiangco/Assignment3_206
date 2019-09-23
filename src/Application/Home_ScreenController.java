ded98556dd847cbd6ae93e798f2c5f83113c9d52
Yuansheng Zhang <yzhb120@en-337239.uoa.auckland.ac.nz>
2019-09-23 18:50:44 +1200

    added Creation class

--- src/Application/Home_ScreenController.java
+++ src/Application/Home_ScreenController.java
@@ -1,24 +1,35 @@
 package Application;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Home_ScreenController extends Controller implements Initializable {
 
 
-    @FXML
-    private Button _playButton;
+    @FXML private Button _playButton;
     @FXML private Button _addButton;
     @FXML private Button _deleteButton;
     @FXML private Button _settingsButton;
+    @FXML private TableView _creationTable;
+    @FXML private TableColumn _nameColumn;
+    @FXML private TableColumn _termSearchedColumn;
+    @FXML private TableColumn _dateModifiedColumn;
+    @FXML private TableColumn _videoLengthColumn;
+    private UpdateHelper _updateHelper;
+    private ArrayList<Creation> _creations = new ArrayList<Creation>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        _updateHelper = new UpdateHelper(this.getCurrentController());
+        UpdateHelper.run();
     }
 
     @FXML
@@ -39,5 +50,9 @@ public class Home_ScreenController extends Controller implements Initializable {
     @FXML
     public void handleSettings() {
         System.out.println("You pressed settings");
+    }
+
+    public ArrayList<Creation> getCreations() {
+        return _creations;
     }
 }
