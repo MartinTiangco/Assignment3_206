1c24512bccf1428b0b061bfa08b8ac8b13c4b774
Martin <martintiangco@gmail.com>
2019-09-24 21:42:47 +1200

    Prepared the controller for the Add_Audio_Screen

---
+++ src/Application/Add_Audio_ScreenController.java
@@ -0,0 +1,13 @@
+package Application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Add_Audio_ScreenController extends Controller {
+	@FXML private Button _mainMenuButton;
+	
+	@FXML
+	public void handleBackToMainMenu() {
+		System.out.println("You pressed back to main menu button.");
+	}
+}
--- src/Application/Home_ScreenController.java
+++ src/Application/Home_ScreenController.java
@@ -2,11 +2,15 @@ package Application;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -43,8 +47,24 @@ public class Home_ScreenController extends Controller implements Initializable {
 
     @FXML
     public void handleAdd() {
-
-        System.out.println("You pressed add");
+    	Stage addAudioStage = new Stage();
+        try {
+        	FXMLLoader loader = new FXMLLoader(getClass().getResource("Add_Audio_Screen.fxml"));
+            Parent root = loader.load();
+            Add_Audio_ScreenController Add_Audio_ScreenController = loader.getController();
+           	Add_Audio_ScreenController.setCurrentController(Add_Audio_ScreenController);
+            Scene scene = new Scene(root, 858, 692);
+            
+            //once we have the css file for Add_Audio_Screen
+            //scene.getStylesheets().addAll(this.getClass().getResource("css/Add_Audio_Screen.css").toExternalForm());
+            
+            addAudioStage.setScene(scene);
+            addAudioStage.show();	
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        
+        Update();
     }
 
     @FXML
