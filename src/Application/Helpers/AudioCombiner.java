0b362c1713330df5b06d060d8816a4adddabb73d
Martin Tiangco <mtia116@en-337222.uoa.auckland.ac.nz>
2019-09-28 12:43:28 +1200

    Fixed disable play and create buttons

--- src/Application/Helpers/AudioCombiner.java
+++ src/Application/Helpers/AudioCombiner.java
@@ -6,16 +6,24 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import Application.Controllers.Add_Audio_ScreenController;
+import Application.Controllers.Controller;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class AudioCombiner extends Task<Long> {
 	private List<Audio> _audioList;
+	private Add_Audio_ScreenController _controller;
 	private final String AUDIO_DIR = ".Audio_Directory" + System.getProperty("file.separator");
 	
-	public AudioCombiner(ObservableList<Audio> audioList) {
+	public AudioCombiner(ObservableList<Audio> audioList, Add_Audio_ScreenController controller) {
 		_audioList = audioList;
+		_controller = controller;
 	}
 	
 	@Override
@@ -37,6 +45,32 @@ public class AudioCombiner extends Task<Long> {
             int exitStatus = process.waitFor();
             System.out.println("Exit Status for sox is " + exitStatus);
             if (exitStatus == 0) {
+            	Runnable runnable = new Runnable() {
+
+					@Override
+					public void run() {
+						Stage imageScreen = new Stage();
+		        		try {
+		        			FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/Image_Selection_Screen.fxml"));
+		        	        Parent root = loader.load();
+		        	        Controller Image_Selection_ScreenController = loader.getController();
+		        	        Image_Selection_ScreenController.setCurrentController(Image_Selection_ScreenController);
+		        	        Image_Selection_ScreenController.setParentController(_controller);
+		        	        imageScreen.setTitle("VARpedia - Image Selection Screen");
+		        	        Scene scene = new Scene(root, 700, 600);
+		        	        //scene.getStylesheets().addAll(this.getClass().getResource("css/Home_Screen.css").toExternalForm());
+		        	        imageScreen.setScene(scene);
+		        	        imageScreen.show();
+		        		} catch (Exception e) {
+		        			e.printStackTrace();
+		        		}
+		        		
+		        		// closes the audio screen
+		        		Stage stage = (Stage) _controller.getAudioList().getScene().getWindow();
+		                stage.close();
+					}          		
+            	};
+            	Platform.runLater(runnable);
             } else {
                 AlertMessage alert = new AlertMessage("audio_combining_failed");
                 Platform.runLater(alert);
