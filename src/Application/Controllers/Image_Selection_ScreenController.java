b248984e76f3886fd0c9687e3bb26f5e28f355a8
Will-ZYS <willyzhysh@gmail.com>
2019-10-12 21:47:54 +1300

    prepare for pull

--- src/Application/Controllers/Image_Selection_ScreenController.java
+++ src/Application/Controllers/Image_Selection_ScreenController.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Executors;
 
 import Application.Helpers.ImageGenerator;
 import Application.Helpers.ImageViewer;
+import Application.Helpers.Picture;
 import Application.Helpers.VideoGenerator;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -15,146 +16,107 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.util.Callback;
 
 public class Image_Selection_ScreenController extends Controller {
-	
-	private final String IMAGE_DIR = ".Image_Directory" + System.getProperty("file.separator");
-	
+
+	private static final String IMAGE_DIR = ".Image_Directory" + System.getProperty("file.separator");
+
 	@FXML private Button _createButton;
 	@FXML private Button _generateButton;
 	@FXML private Label _invalidInput;
-	@FXML private ListView<Image> _listOfImages;
+	@FXML private ListView<Picture> _listOfImages;
 	@FXML private ImageView _imageView = new ImageView();
 	@FXML private AnchorPane _entireScreenPane;
 	@FXML private ProgressIndicator _progressIndicator;
 	@FXML private TextField _nameInput;
 	@FXML private TextField _input;
 	@FXML private CheckBox _selectAll;
-	
+	@FXML private ListView<ImageView> _selectedImages;
+
 	private Add_Audio_ScreenController _controller;
 	private ExecutorService _executor = Executors.newSingleThreadExecutor();
 	private String _term;
-	
+
 	public void initialize() {
 		_generateButton.setDisable(true);
-		
+
+
 		_input.textProperty().addListener(new ChangeListener<String>() {
-		    @Override
-		    public void changed(ObservableValue<? extends String> observable, String oldValue, 
-		        String newValue) {
-		    	// only allow digits to be typed
-		        if (!newValue.matches("\\d+")) {
-		            _input.setText(newValue.replaceAll("[^\\d]", ""));
-		        }
-		        // only allow up to 2 digits to be typed
-		        if (newValue.length() > 2) {
-		        	_input.setText(newValue.substring(0,2));
-		        }
-		        // enables or disables button if number is a valid input
-		        if (!_input.getText().isEmpty()) {
-		        	if (_input.getText().matches("\\d+")) {
-		        		if (isValidNumber()) {
-				        	_invalidInput.setVisible(false);
-				        	_generateButton.setDisable(false);
-				        } else {
-				        	_invalidInput.setVisible(true);
-				        	_generateButton.setDisable(true);
-				        }
-		        	}
-		        } else {
-		        	// if textField is empty, disable the button.
-		        	_generateButton.setDisable(true);
-		        }
-		    }
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue,
+								String newValue) {
+				// only allow digits to be typed
+				if (!newValue.matches("\\d+")) {
+					_input.setText(newValue.replaceAll("[^\\d]", ""));
+				}
+				// only allow up to 2 digits to be typed
+				if (newValue.length() > 2) {
+					_input.setText(newValue.substring(0,2));
+				}
+				// enables or disables button if number is a valid input
+				if (!_input.getText().isEmpty()) {
+					if (_input.getText().matches("\\d+")) {
+						if (isValidNumber()) {
+							_invalidInput.setVisible(false);
+							_generateButton.setDisable(false);
+						} else {
+							_invalidInput.setVisible(true);
+							_generateButton.setDisable(true);
+						}
+					}
+				} else {
+					// if textField is empty, disable the button.
+					_generateButton.setDisable(true);
+				}
+			}
 		});
 
 		// handles the checkboxes next to the images
-        _listOfImages.setCellFactory(CheckBoxListCell.forListView(new Callback<Image, ObservableValue<Boolean>>() {
-            @Override
-            public ObservableValue<Boolean> call(Image image) {
-				image.onProperty().addListener((obs, wasOn, isNowOn) -> {
-					image.setSelected(isNowOn);
-            });
-				return image.onProperty();
-            }
-        }));
+		_listOfImages.setCellFactory(CheckBoxListCell.forListView(image -> {
+			image.onProperty().addListener((obs, wasOn, isNowOn) -> {
+				image.setSelected(isNowOn);
+				updateSelectedImages();
+				if (!isNowOn) {
+					_selectAll.setSelected(false);
+				}
+			});
+			return image.onProperty();
+		}));
 	}
 
+
+
+
+
 	// this is needed for the check box functionality
-	public static class Image {
-        private final StringProperty name = new SimpleStringProperty();
-        private final BooleanProperty on = new SimpleBooleanProperty();
-        private String fileName = "";
-        private Boolean selected = false;
 
-        public Image(String name, boolean on) {
-            setName(name);
-            setOn(on);
-        }
-
-        public final StringProperty nameProperty() {
-            return this.name;
-        }
-
-        public final String getName() {
-            return this.nameProperty().get();
-        }
-
-        public final void setName(final String name) {
-            this.nameProperty().set(name);
-        }
-
-        public final BooleanProperty onProperty() {
-            return this.on;
-        }
-
-        public final void setOn(final boolean on) {
-            this.onProperty().set(on);
-        }
-
-		public String getFileName() {
-			return fileName;
-		}
-
-		public void setFileName(String fileName) {
-			this.fileName = fileName;
-		}
-
-		public Boolean getSelected() {
-			return selected;
-		}
-
-		public void setSelected(Boolean selected) {
-			this.selected = selected;
-		}
-
-		@Override
-        public String toString() {
-            return getName();
-        }
-
-    }
-	
 	public void handleGenerate() {
 		if (!isValidNumber()) {
 			return;
 		}
-		
+
 		// progress indicator
 		_entireScreenPane.setDisable(true);
 		_progressIndicator.setProgress(-1);
 		_progressIndicator.setVisible(true);
-		
+
 		_term = ((Add_Audio_ScreenController)(this.getParentController())).getSearchInput();
 		int numPics = Integer.parseInt(_input.getText());
-		
+
 		// retrieves images from Flickr
 		ImageGenerator imgGen = new ImageGenerator(_term, numPics, this);
 		_executor.submit(imgGen);
@@ -167,8 +129,7 @@ public class Image_Selection_ScreenController extends Controller {
 
 		if (listOfFiles != null) {
 			for (int i = 0; i < listOfFiles.length; i++) {
-				Image image = new Image( _term + " " + (i+1), false);
-				image.setFileName(listOfFiles[i].getName());
+				Picture image = new Picture("file:" + listOfFiles[i].getAbsolutePath(),_term + " " + (i+1), false, listOfFiles[i].getName());
 				_listOfImages.getItems().add(image);
 			}
 		}
@@ -181,28 +142,39 @@ public class Image_Selection_ScreenController extends Controller {
 
 	public void selectAll() {
 		if (_selectAll.isSelected()) {
-			for (Image image : _listOfImages.getItems()) {
+			for (Picture image : _listOfImages.getItems()) {
 				image.setSelected(true);
 				image.setOn(true);
 			}
 		}
 		else {
-			for (Image image : _listOfImages.getItems()) {
+			for (Picture image : _listOfImages.getItems()) {
 				image.setSelected(false);
 				image.setOn(false);
 			}
 		}
-
+		updateSelectedImages();
 	}
 
 	public void viewImage(){
 		if (_listOfImages.getSelectionModel().getSelectedItem() != null) {
-			String imagePath = "file:" + IMAGE_DIR + _listOfImages.getSelectionModel().getSelectedItem().getFileName();
-			javafx.scene.image.Image image = new javafx.scene.image.Image(imagePath);
-			_imageView.setImage(image);
+			_imageView.setImage(_listOfImages.getSelectionModel().getSelectedItem());
 		}
 	}
-	
+
+	public void updateSelectedImages() {
+		for (Picture image : _listOfImages.getItems()) {
+			if (image.getSelected() && !_selectedImages.getItems().contains(image.getImageView())) {
+				_selectedImages.getItems().add(image.getImageView());
+			}
+			else if(!image.getSelected() && _selectedImages.getItems().contains(image.getImageView())){
+				_selectedImages.getItems().remove(image.getImageView());
+			}
+
+		}
+	}
+
+
 	public void handleCreate() {
 		if (!isNameValid()) {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -214,16 +186,14 @@ public class Image_Selection_ScreenController extends Controller {
 		}
 		// disallows user to click Create again
 		_createButton.setDisable(false);
-				
+
 		// creates the creation
 		VideoGenerator videoGen = new VideoGenerator(_term, this);
 		videoGen.setCreationName(_nameInput.getText());
 		int numPics = 0;
-		for (Image image : _listOfImages.getItems()) {
-			if (image.getSelected()){
-				numPics++;
-				videoGen.addImage(IMAGE_DIR + image.getFileName());
-			}
+		for (ImageView imageView : _selectedImages.getItems()) {
+			videoGen.addImage(IMAGE_DIR + ((Picture)imageView.getImage()).getFileName());
+			numPics++;
 		}
 		if (numPics == 0) {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -237,19 +207,20 @@ public class Image_Selection_ScreenController extends Controller {
 		_executor.submit(videoGen);
 	}
 
+
 	public boolean isValidNumber() {
 		if (Integer.parseInt(_input.getText()) > 0 && Integer.parseInt(_input.getText()) < 11) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	private boolean isNameValid() {
 		// Disallows input of spaces or an empty string
 		if (_nameInput.getText().trim().isEmpty()) {
 			return false;
 		}
-		
+
 		//prevents any special characters apart from "-", "_" and (space)
 		if (_nameInput.getText().matches("[a-zA-Z0-9 ]*")) {
 			return true;
@@ -261,16 +232,16 @@ public class Image_Selection_ScreenController extends Controller {
 	public Button getCreateButton() {
 		return _createButton;
 	}
-	
+
 	public AnchorPane getEntireScreenPane() {
 		return _entireScreenPane;
 	}
-	
+
 	public ProgressIndicator getProgressIndicator() {
 		return _progressIndicator;
 	}
 
-	public ListView<Image> getListOfImages() {
+	public ListView<Picture> getListOfImages() {
 		return _listOfImages;
 	}
 
