d56e22f323c344bd79d8946a1dad7ef4ee30fd0e
Martin <martintiangco@gmail.com>
2019-10-23 21:45:07 +1300

    Added better documentation to Cleaner

--- src/Application/Helpers/Cleaner.java
+++ src/Application/Helpers/Cleaner.java
@@ -4,22 +4,35 @@ import java.io.File;
 
 /**
  * Class to clean up and remove temp files in directories
+ * @author Group 25:
+ * 			- Martin Tiangco, mtia116
+ * 			- Yuansheng Zhang, yzhb120
  */
 public class Cleaner {
 
+	// the directories
     private final File WIKIT_DIR = new File(".Wikit_Directory" + System.getProperty("file.separator"));
     private final File AUDIO_DIR = new File(".Audio_Directory" + System.getProperty("file.separator"));
     private final File OUTPUT_DIR = new File(".Output_Directory" + System.getProperty("file.separator"));
     private final File CREATION_DIR = new File("Creation_Directory" + System.getProperty("file.separator"));
     private final File IMAGE_DIR = new File(".Image_Directory" + System.getProperty("file.separator"));
 
-
     public final void cleanAll() {
         cleanWikit();
         cleanAudio();
         cleanImage();
         cleanOutput();
-//        cleanCreation();
+    }
+    
+    public final void cleanCreation(String folderName) {
+    	File folder = new File(CREATION_DIR + System.getProperty("file.separator") + folderName);
+    	System.out.println(folder.toString());
+        File[] files = folder.listFiles();
+        if(files != null) {
+            for (File f: files) {
+                f.delete();
+            }
+        }
     }
 
     public final void cleanWikit() {
@@ -42,17 +55,6 @@ public class Cleaner {
 
     public final void cleanOutput() {
         File[] files = OUTPUT_DIR.listFiles();
-        if(files != null) {
-            for (File f: files) {
-                f.delete();
-            }
-        }
-    }
-
-    public final void cleanCreation(String folderName) {
-    	File folder = new File(CREATION_DIR + System.getProperty("file.separator") + folderName);
-    	System.out.println(folder.toString());
-        File[] files = folder.listFiles();
         if(files != null) {
             for (File f: files) {
                 f.delete();
