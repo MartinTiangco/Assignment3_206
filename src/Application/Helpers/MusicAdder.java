dd4f9fea64f9298531f2b6987396b749a7a87cb8
Martin <martintiangco@gmail.com>
2019-10-24 00:22:07 +1300

    Added better documentation to MusicAdder

--- src/Application/Helpers/MusicAdder.java
+++ src/Application/Helpers/MusicAdder.java
@@ -7,15 +7,19 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.stage.Stage;
 
+/**
+ * Class to add the background music into the output audio file
+ * @author Group 25:
+ * 			- Martin Tiangco, mtia116
+ * 			- Yuansheng Zhang, yzhb120
+ */
 public class MusicAdder extends Task<Long> {
-	
-	private final String AUDIO_DIR = ".Audio_Directory" + System.getProperty("file.separator");
 	private final String MUSIC_DIR = ".Music_Directory" + System.getProperty("file.separator");
 	private final String OUTPUT_DIR = ".Output_Directory" + System.getProperty("file.separator");
-	
-	private Track _chosenTrack;
-	private String _spokenFile;
+
 	private Background_Music_ScreenController _controller;
+	private String _spokenFile; // the file that is sent through text-to-speech
+	private Track _chosenTrack; // the music track that the user selected
 	
 	public MusicAdder(Track chosenTrack, String spokenFile, Background_Music_ScreenController controller) {
 		_chosenTrack = chosenTrack;
@@ -31,6 +35,7 @@ public class MusicAdder extends Task<Long> {
 		
 		String cmd = "";	
 		if (_chosenTrack.getTrackName().equals(_chosenTrack.getNoMusicString())) {
+			// no background music is chosen
 			cmd = "ffmpeg -i " + spokenAudio + " " + output;
 		} else {
 			String bgMusic = MUSIC_DIR + _chosenTrack.getTrackName();
@@ -38,10 +43,8 @@ public class MusicAdder extends Task<Long> {
 		}
 		
 		ProcessBuilder builder = new ProcessBuilder("bash", "-c", cmd);
-		System.out.println(cmd);
-		Process process;
 		try {
-			process = builder.start();
+			Process process = builder.start();
 			int exitStatus = process.waitFor();
 			if (exitStatus == 0) {
 				Runnable runnable = new Runnable() {
@@ -50,13 +53,14 @@ public class MusicAdder extends Task<Long> {
 						// loads the Image Selection Screen
 						_controller.loadScreen("Image Selection Screen", "/Application/fxml/Image_Selection_Screen.fxml", "");
 
-						// closes the Add Audio screen
+						// closes the 'Background Music Screen'
 						Stage stage = (Stage) _controller.getNextButton().getScene().getWindow();
-						stage.close();
+						stage.hide();
 					}          		
 				};
 				Platform.runLater(runnable);
 			} else {
+				// if the background music fails to combine with the audio
 				AlertMessage alert = new AlertMessage("bgm_failed");
 				Platform.runLater(alert);
 			}
